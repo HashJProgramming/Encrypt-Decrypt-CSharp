29555bff02dc59f5f1dab3b5e6c618d1bb1dc17a
Joshua Ambalong <JoshuaMarceloAmbalong@gmail.com>
2021-08-27 21:52:46 +0800

    Add project files.

---
+++ AES 256 CSharp/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+
+
+namespace AES_256_CSharp
+{
+    class Program
+    {
+        public static string EncryptionKey = "hashjprogramming";
+        public static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Title = "Hash'J Programming";
+            Console.WindowWidth = 80;
+            Console.WriteLine("=============================== HashJProgramming ===============================");
+            Console.WriteLine("ENCRYPTED: {0}",Encrypt("HashJProgramming"));
+            Console.WriteLine("DECRYPTED: {0}",Decrypt(Encrypt("HashJProgramming")));
+            Console.WriteLine("ENCRYPTION KEY: {0}", EncryptionKey);
+            Console.ReadLine();
+        }
+        public static string Encrypt(string clearText)
+        {
+            
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    clearText = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return clearText;
+        }
+        public static string Decrypt(string cipherText)
+        {
+            
+            cipherText = cipherText.Replace(" ", "+");
+            byte[] cipherBytes = Convert.FromBase64String(cipherText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return cipherText;
+        }
+
+
+
+    }
+}
